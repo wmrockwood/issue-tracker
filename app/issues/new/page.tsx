0481d1c865e56d0a9ab5fa2ac7e0670de89ec780
Bill Rockwood <william.rockwood@gmail.com>
2024-03-06 09:17:18 -0500

    build new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { TextArea, TextField } from '@radix-ui/themes';
+
+const NewIssuePage = () => {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root>
+        <TextField.Input placeholder="Title" />
+      </TextField.Root>
+      <TextArea placeholder="description" />
+    </div>
+  );
+};
+export default NewIssuePage;
