f6614a092ccc4c5b6c0d159dd45eaec25fe0ece3
Bill Rockwood <william.rockwood@gmail.com>
2024-03-07 10:36:05 -0500

    style issue details

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,21 +1,28 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import prisma from '@/prisma/client';
+import { Card, Flex, Heading } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
+import { Text } from '@radix-ui/themes';
 
 interface Props {
   params: { id: string };
 }
 
 const IssueDetailPage = async ({ params: { id } }: Props) => {
-  if (typeof id !== 'number') notFound();
+  if (isNaN(parseInt(id))) notFound();
   const issue = await prisma.issue.findUnique({ where: { id: Number(id) } });
 
   if (!issue) notFound();
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex gap="3" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        <Text>{issue.description}</Text>
+      </Card>
     </div>
   );
 };
