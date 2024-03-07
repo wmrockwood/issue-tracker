e03f142c398f8a09c76fc49ef177a829b281478c
Bill Rockwood <william.rockwood@gmail.com>
2024-03-07 12:30:35 -0500

    add edit button

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,15 @@
 'use client';
-import { Button, Callout, Text, TextField } from '@radix-ui/themes';
-import dynamic from 'next/dynamic';
-import { useForm, Controller } from 'react-hook-form';
+import { ErrorMessage, Spinner } from '@/app/components';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
+import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchemas';
+import { Controller, useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { ErrorMessage, Spinner } from '@/app/components';
 
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
   ssr: false,
