72dd6fc96590cf2dfe8a3cf8af420eece9934ec6
Bill Rockwood <william.rockwood@gmail.com>
2024-03-07 12:50:14 -0500

    refactor issue display

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,20 @@
+import { IssueStatusBadge } from '@/app/components';
+import { Heading, Flex, Card, Text } from '@radix-ui/themes';
+import Markdown from 'react-markdown';
+import { Issue } from '@prisma/client';
+
+const IssueDetails = ({ issue }: { issue: Issue }) => {
+  return (
+    <>
+      <Heading>{issue.title}</Heading>
+      <Flex gap="3" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose mt-4">
+        <Markdown>{issue.description}</Markdown>
+      </Card>
+    </>
+  );
+};
+export default IssueDetails;
