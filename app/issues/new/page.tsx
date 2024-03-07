3036f9df84264d6d24c3c93d42373e228654d70d
Bill Rockwood <william.rockwood@gmail.com>
2024-03-07 08:54:34 -0500

    refactoring issue submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -27,6 +27,17 @@ const NewIssuePage = () => {
   const [error, setError] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setIsSubmitting(true);
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    } catch (error) {
+      setIsSubmitting(false);
+      setError('an unexpected error occurred');
+    }
+  });
+
   return (
     <div className="max-w-xl space-y-3">
       {error && (
@@ -34,18 +45,7 @@ const NewIssuePage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setIsSubmitting(true);
-            await axios.post('/api/issues', data);
-            router.push('/issues');
-          } catch (error) {
-            setIsSubmitting(false);
-            setError('an unexpected error occurred');
-          }
-        })}
-      >
+      <form onSubmit={onSubmit}>
         <TextField.Root className="mb-3">
           <TextField.Input placeholder="Title" {...register('title')} />
         </TextField.Root>
