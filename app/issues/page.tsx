d8775c1f9029c30f6c021b86c4a9ce99956132f2
Bill Rockwood <william.rockwood@gmail.com>
2024-03-07 11:47:45 -0500

    styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,8 @@
-import { Button, Table } from '@radix-ui/themes';
-import Link from 'next/link';
+import { Table } from '@radix-ui/themes';
 import prisma from '@/prisma/client';
 import IssueStatusBadge from '../components/IssueStatusBadge';
 import IssueActions from './IssueActions';
+import Link from '../components/Link';
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
