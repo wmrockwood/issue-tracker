515db15f3fa73e8736d60135719cd96a1c26c883
Bill Rockwood <william.rockwood@gmail.com>
2024-03-11 12:48:49 -0400

    changed page structure

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -13,7 +13,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: string }) => {
     try {
       setDeleting(true);
       await axios.delete(`/api/issues/${issueId}`);
-      router.push('/issues');
+      router.push('/issues/list');
       router.refresh();
     } catch (error) {
       setDeleting(false);
---
+++ app/issues/edit/page.tsx
@@ -0,0 +1,22 @@
+import { notFound } from 'next/navigation';
+import prisma from '../../../prisma/client';
+import dynamic from 'next/dynamic';
+import IssueFormSkeleton from './[id]/loading';
+
+const IssueForm = dynamic(() => import('../_components/IssueForm'), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
+
+interface Props {
+  params: { id: string };
+}
+
+const EditIssuePage = async ({ params: { id } }: Props) => {
+  if (isNaN(parseInt(id))) notFound();
+  const issue = await prisma.issue.findUnique({ where: { id: Number(id) } });
+
+  if (!issue) notFound();
+  return <IssueForm issue={issue} />;
+};
+export default EditIssuePage;
