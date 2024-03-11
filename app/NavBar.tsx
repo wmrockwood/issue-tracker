86c8bf993f96fe55157c5dd76d0ef9316e47242d
Bill Rockwood <william.rockwood@gmail.com>
2024-03-11 17:22:27 -0400

    change referrer policy

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -55,6 +55,7 @@ const NavBar = () => {
                     size="2"
                     radius="full"
                     className="cursor-pointer"
+                    referrerPolicy="no-referrer"
                   />
                 </DropdownMenu.Trigger>
                 <DropdownMenu.Content>
