4de63b6798d98b9a61730c0d39709ba3c2cde9d0
Bill Rockwood <william.rockwood@gmail.com>
2024-03-11 17:42:18 -0400

    refactor nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -12,66 +12,76 @@ import {
   Text,
 } from '@radix-ui/themes';
 
-const NavBar = () => {
+const NavMenu = () => {
   const currentPath = usePathname();
-  const { status, data: session } = useSession();
+
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues/list' },
   ];
+  return (
+    <Flex align="center" gap="3">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.label}>
+            <Link
+              className={`nav-link ${
+                currentPath === link.href && 'text-zinc-900'
+              }`}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Flex>
+  );
+};
 
+const UserDropdown = () => {
+  const { status, data: session } = useSession();
+  if (status === 'loading') return null;
+  if (status === 'unauthenticated')
+    return <Link href="/api/auth/signin">Sign In</Link>;
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link className="nav-link" href="/api/auth/signout">
+              Sign Out
+            </Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+const NavBar = () => {
   return (
     <nav className="border-b mb-5 px-5 py-3">
       <Container>
         <Flex justify="between">
-          <Flex align="center" gap="3">
-            <Link href="/">
-              <AiFillBug />
-            </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.label}>
-                  <Link
-                    className={`${
-                      currentPath === link.href
-                        ? 'text-zinc-900'
-                        : 'text-zinc-500'
-                    } hover:text-zinc-800 transition-colors`}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Flex>
-          <Box>
-            {status === 'authenticated' && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                    referrerPolicy="no-referrer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Sign Out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === 'unauthenticated' && (
-              <Link href="/api/auth/signin">Sign In</Link>
-            )}
-          </Box>
+          <NavMenu />
+          <UserDropdown />
         </Flex>
       </Container>
     </nav>
