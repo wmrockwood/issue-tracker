fae43cb358fb4ec35af7ef83f0e544e222d19981
Bill Rockwood <william.rockwood@gmail.com>
2024-03-13 10:57:40 -0400

    refactor assignee select

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,38 +1,32 @@
 'use client';
+import { Skeleton } from '@/app/components';
 import { Issue, User } from '@prisma/client';
 import { Select, Text } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { Skeleton } from '@/app/components';
 import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then((res) => res.data),
-    staleTime: 60 * 1000,
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />;
   if (error) return null;
+
+  const assignIssue = async (userId: string) => {
+    try {
+      await axios.patch(`/api/issues/${issue.id}`, {
+        assignedToUserId: userId || null,
+      });
+    } catch (error) {
+      toast.error('Changes could not be saved');
+    }
+  };
 
   return (
     <>
       <Text>Assign to:</Text>
       <Select.Root
-        onValueChange={async (userId: string) => {
-          try {
-            await axios.patch(`/api/issues/${issue.id}`, {
-              assignedToUserId: userId || null,
-            });
-          } catch (error) {
-            toast.error('Changes could not be saved');
-          }
-        }}
+        onValueChange={assignIssue}
         defaultValue={issue.assignedToUserId || ''}
       >
         <Select.Trigger />
@@ -52,4 +46,13 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
+
 export default AssigneeSelect;
